63759600caf61c4f2f82029899317429c4b4c527
KenjiX1 <145561863+kenjixx1@users.noreply.github.com>
2025-04-04 20:34:12 +0700

    Little edit

--- ChatGui/ChatGui/ChatGui.cpp
+++ ChatGui/ChatGui/ChatGui.cpp
@@ -22,9 +22,11 @@ ChatGui::ChatGui(QWidget *parent)
     ui.StackedChatFrame->addWidget(FirstPage->page);
     ui.StackedChatFrame->setCurrentWidget(FirstPage->page);
     CurrentChatFrame = FirstPage;
+    CurrentChatFrame->AddFMessage("What can I help you with?", 0);
 	current_session = nullptr; 
     const char* dbPath = "chatHistory.db"; // Path to database
     int rc;
+
 
 	// Check if the database file exists and create tables else open the database
 	if (!QFile::exists(dbPath)) {
@@ -34,6 +36,8 @@ ChatGui::ChatGui(QWidget *parent)
     else {
 		rc = sqlite3_open(dbPath, &db);
     }
+
+
 
 
     DeactiveSS = "QPushButton{\n""background-color:#2aa5ff;\n""color: rgb(255, 255, 255);\n""border-radius:0px;\n""}\n""QPushButton::hover{\n""background-color:#0865c5;\n"
@@ -55,8 +59,29 @@ ChatGui::ChatGui(QWidget *parent)
         int visline = qMin(qMax(currentline, min), max);
         int newheight = visline * height + pad;
         ui.textEdit->setFixedHeight(newheight);
+
+		//QMessageBox::information(nullptr, "Message", QString::number(currentline));
+
+		QTextCursor cursor = ui.textEdit->textCursor();
+		int cursorPos = cursor.position();
+
+		cursor.movePosition(QTextCursor::StartOfLine,QTextCursor::MoveAnchor);
+		int startOfLine = cursor.position();
+        cursor.movePosition(QTextCursor::EndOfLine, QTextCursor::KeepAnchor);
+        QString currentLineText = cursor.selectedText();
+
+		
+        QString text = ui.textEdit->toPlainText();
+        int width = ui.textEdit->width();
+		int currentWidth = matric.horizontalAdvance(currentLineText);
+		if (currentWidth > width * 0.9) {
+            ui.textEdit->insertPlainText("\n");
+            ui.textEdit->moveCursor(QTextCursor::End);
+		}
+		
+        
+        
         });
-    AddMessage("What can I help you with?", 0);
 
 }
 
@@ -86,7 +111,8 @@ void ChatGui::ActiveButton_Click() {
     clist.SetActive(id);
     ActiveButton = clist.GetButtonByID(id);
     ClearLayout(ui.verticalLayout_3);
-
+    ui.StackedChatFrame->setCurrentIndex(id+1);
+    
 }
 
 void ChatGui::ClearLayout(QLayout* layout) {
@@ -112,6 +138,14 @@ void ChatGui::on_DeleteButton_clicked() {
         int id = ActiveButton->property("ChatId").toInt();
         clist.Remove(id);
         LoadChatHistory();
+		ui.StackedChatFrame->removeWidget(CurrentChatFrame->page);
+		delete CurrentChatFrame;
+		ChatFrame* newpage = new ChatFrame();
+		ui.StackedChatFrame->addWidget(newpage->page);
+		ui.StackedChatFrame->setCurrentWidget(newpage->page);
+		CurrentChatFrame = newpage;
+        CurrentChatFrame->AddFMessage("What can I help you with?", 0);
+		
     }
     ActiveButton = nullptr;
     
@@ -124,15 +158,16 @@ void ChatGui::on_SendButton_clicked() {
         font1.setPointSize(15);
         font1.setBold(true);
         font1.setWeight(75);
-        QPushButton* TempTest;
-        TempTest = new QPushButton(ui.scrollAreaWidgetContents_3);
-        TempTest->setFont(font1);
-        TempTest->setStyleSheet(DeactiveSS);
+        Session* TempTest;
+        string something;
+        TempTest = new Session(0,"Kenji",something,0,"",ui.scrollAreaWidgetContents_3);
+        /*TempTest->setFont(font1);
+        TempTest->setStyleSheet(DeactiveSS);*/
+        /*TempTest->setMinimumSize(QSize(100, 25));*/
 
-        TempTest->setMinimumSize(QSize(100, 25));
         ui.verticalLayout_2->addWidget(TempTest, 0, Qt::AlignTop);
 
-        connect(TempTest, &QPushButton::clicked, this, &::ChatGui::ActiveButton_Click);
+        /*connect(TempTest, &QPushButton::clicked, this, &::ChatGui::ActiveButton_Click);
         int size = clist.Size();
         TempTest->setProperty("ChatId", clist.GetMyID());
         int id = clist.GetMyID();
@@ -140,8 +175,7 @@ void ChatGui::on_SendButton_clicked() {
         TempTest->setText(name);
         clist.AddList(TempTest);
         ActiveButton = TempTest;
-        clist.SetActive(id);
-
+        clist.SetActive(id);*/
 
 
     }
@@ -199,65 +233,7 @@ void ChatGui::on_RenameButton_clicked() {
 
 
 void ChatGui::AddMessage(QString message, bool sender) {
-    //Bot=0 User=1 :D
-    //CurrentChatFrame->AddFMessage(message, sender);
-    
-    //QFrame* tempFrame = new QFrame(ui.scrollAreaWidgetContents_2);
-    //tempFrame->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Maximum);
-    //tempFrame->setStyleSheet(QString::fromUtf8("background-color:#1f2023;"));
-    //tempFrame->setFrameShape(QFrame::StyledPanel);
-
-
-
-    //QHBoxLayout* layout = new QHBoxLayout(tempFrame);
-    //layout->setSpacing(6);
-    //layout->setContentsMargins(10, 10, 10, 10);
-
-    //
-    //
-    //QFont font;
-    //font.setPointSize(14);
-
-    //QLabel* text = new QLabel(message);
-    //text->setMaximumWidth(ui.TestMessageScrollArea->width()*0.8);
-    //text->setFont(font);
-    //text->setTextInteractionFlags(Qt::TextBrowserInteraction);
-    //text->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Maximum);
-    //text->setStyleSheet(QString::fromUtf8("color: rgb(255, 255, 255);"));
-    //
-    //QTimer::singleShot(10, this, [=]() {
-    //    if (text->width() > ui.TestMessageScrollArea->width() * 0.7) {
-    //        text->setMinimumWidth(ui.TestMessageScrollArea->width() * 0.5);
-    //        text->setWordWrap(true);
-    //        //QMessageBox::information(nullptr, "Y", "Text:" + QString::number(text->width()) + "|| Frame:" + QString::number(ui.TestMessageScrollArea->width() * 0.7));
-    //        
-    //    }
-    //    else {
-    //        //QMessageBox::information(nullptr, "X", "Text:" + QString::number(text->width()) + "|| Frame:" + QString::number(ui.TestMessageScrollArea->width() * 0.7));
-    //        text->setWordWrap(false);
-    //    }
-    //    }
-    //);
-    //
-    //
-    //if (sender) {
-    //    layout->addStretch();
-    //    layout->addWidget(text,0,Qt::AlignCenter);
-    //    ui.verticalLayout_5->addWidget(tempFrame, 0, Qt::AlignRight);
-    //}
-    //else {
-    //    layout->addWidget(text, 0, Qt::AlignCenter);
-    //    layout->addStretch();
-    //    ui.verticalLayout_5->addWidget(tempFrame, 0, Qt::AlignLeft);
-    //    
-    //}
-    ////QMessageBox::information(nullptr, "Height", QString::number(ui.TestMessageScrollArea->verticalScrollBar()->maximum()));
-
-    //QTimer::singleShot(10, this, [=]() {
-    //    QScrollBar* scrollbar = ui.TestMessageScrollArea->verticalScrollBar();
-    //    scrollbar->setValue(ui.TestMessageScrollArea->verticalScrollBar()->maximum()-1);
-    //    }
-    //);
+	CurrentChatFrame->AddFMessage(message, sender);
 }
 
 void ChatGui::sessionSelected(Session* session, int page) {
