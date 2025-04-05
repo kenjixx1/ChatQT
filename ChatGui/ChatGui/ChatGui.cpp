5faf0b6658a6a67b250da66677de2d1aec11e137
RaphOwO <ramida.lap@gmail.com>
2025-04-05 14:53:31 +0700

    fixed bug

--- ChatGui/ChatGui/ChatGui.cpp
+++ ChatGui/ChatGui/ChatGui.cpp
@@ -155,8 +155,8 @@ void ChatGui::on_DeleteButton_clicked() {
 			}
 		}
        
-		    delete current_session;
-		    delete CurrentChatFrame;
+		delete current_session;
+		delete CurrentChatFrame;
 
         ChatFrame* newpage = new ChatFrame();
         ui.StackedChatFrame->addWidget(newpage);
@@ -192,39 +192,35 @@ void ChatGui::on_SendButton_clicked() {
         CurrentChatFrame = DefaultChatFrame;
         DefaultChatFrame = nullptr;
 
-      //Add User Input To the chat Frame
-        CurrentChatFrame->AddFMessage(message, 1);
-        ui.textEdit->clear();
-        ui.textEdit->setFocus();
-    		ui.SendButton->setEnabled(false);
-		    ui.textEdit->setEnabled(false);
-
+        // Create a new session
         TempTest = new Session(id, "", something, 0, ui.textEdit->toPlainText().toStdString(), ui.scrollAreaWidgetContents_3);
         connect(TempTest, &Session::selected, this, &ChatGui::sessionSelected);
         ui.verticalLayout_2->addWidget(TempTest, 0, Qt::AlignTop);
-        
-		    // Add the message to the chat frame
-        AddMessage(QString::fromStdString(something), 0);
-		    ui.SendButton->setEnabled(true);
-		    ui.textEdit->setEnabled(true);
 
-	    	QString AIResponse = QString::fromStdString(something);
+        // Add the message to the chat frame
+        QString message = ui.textEdit->toPlainText();
+        CurrentChatFrame->AddFMessage(message, 1);
+        ui.textEdit->clear();
+        ui.textEdit->setFocus();
+        AddMessage(QString::fromStdString(something), 0);
+
+        QString AIResponse = QString::fromStdString(something);
 
         // Update the database
-	    	updateMessages(id, message.toStdString(), something);
-	    	updateSessionName(id, TempTest->getName());
-	    	updateHistorySize(id, TempTest->getHistorySize());
+        updateMessages(id, message.toStdString(), something);
+        updateSessionName(id, TempTest->getName());
+        updateHistorySize(id, TempTest->getHistorySize());
 
-	    	// Add the new session to the list
-	     	sessions.push_back(TempTest);
-		    current_session = TempTest;
+        // Add the new session to the list
+        sessions.push_back(TempTest);
+        current_session = TempTest;
     }
     else {
-		    // Add the message to the chat frame
-		    CurrentChatFrame->AddFMessage(message, 1);
-	    	ui.textEdit->clear();
-    		ui.textEdit->setFocus();
-	    	string response = getAIResponse(message.toStdString());
+		// Add the message to the chat frame
+		CurrentChatFrame->AddFMessage(message, 1);
+	    ui.textEdit->clear();
+    	ui.textEdit->setFocus();
+	    string response = getAIResponse(message.toStdString());
         AddMessage(QString::fromStdString(response), 0);
     }
     
@@ -292,7 +288,6 @@ void ChatGui::sessionSelected(Session* session, int page) {
     current_session = session;
     
 	ui.StackedChatFrame->setCurrentIndex(getCurrentIndex() + 1);
-	QMessageBox::information(nullptr, "Message", QString::number(getCurrentIndex() + 1));
 	CurrentChatFrame = qobject_cast<ChatFrame*>(ui.StackedChatFrame->currentWidget());
 	//ui.StackedChatFrame->setCurrentWidget(CurrentChatFrame);    
 	if (CurrentChatFrame == nullptr) {
