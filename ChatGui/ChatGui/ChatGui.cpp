cce258ff89444186ffc6b17b6c5bf78dfcf0b13c
KenjiX1 <145561863+kenjixx1@users.noreply.github.com>
2025-04-05 13:34:29 +0700

    Disable Button When sending message and renable after bot return message

--- ChatGui/ChatGui/ChatGui.cpp
+++ ChatGui/ChatGui/ChatGui.cpp
@@ -183,17 +183,23 @@ void ChatGui::on_SendButton_clicked() {
         CurrentChatFrame = DefaultChatFrame;
         DefaultChatFrame = nullptr;
 
+            //Add User Input To the chat Frame
+        CurrentChatFrame->AddFMessage(message, 1);
+        ui.textEdit->clear();
+        ui.textEdit->setFocus();
+		ui.SendButton->setEnabled(false);
+		ui.textEdit->setEnabled(false);
+
+
 		    // Create a new session
-        TempTest = new Session(sessions.size(), "", something, 0, ui.textEdit->toPlainText().toStdString(), ui.scrollAreaWidgetContents_3);
+        TempTest = new Session(sessions.size(), "", something, 0, message.toStdString(), ui.scrollAreaWidgetContents_3);
         connect(TempTest, &Session::selected, this, &ChatGui::sessionSelected);
         ui.verticalLayout_2->addWidget(TempTest, 0, Qt::AlignTop);
         
 		// Add the message to the chat frame
-        QString message = ui.textEdit->toPlainText();
-        CurrentChatFrame->AddFMessage(message, 1);
-        ui.textEdit->clear();
-        ui.textEdit->setFocus();
         AddMessage(QString::fromStdString(something), 0);
+		ui.SendButton->setEnabled(true);
+		ui.textEdit->setEnabled(true);
 
 		QString AIResponse = QString::fromStdString(something);
 
